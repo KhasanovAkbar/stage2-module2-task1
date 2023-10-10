6465d1b2828c8cf21412cad004b701ec689fa610
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-10-10 11:37:55 +0200

    doPost -> doGet

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,6 +1,5 @@
 package com.example.servlet;
 
-import com.example.User;
 import com.example.Warehouse;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
 
 @WebServlet("/users")
 public class GetUsersServlet extends HttpServlet {
